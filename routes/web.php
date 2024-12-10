62e1b431699fd716c19a1e0468cc1b2b1360c2f1
ayutrii <triayuw46@gmail.com>
2024-12-10 04:55:24 +0000

    pertemuan 28

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\barangMasukController;
 use App\Http\Controllers\dashboardController;
 use App\Http\Controllers\pegawaiController;
 use App\Http\Controllers\pelangganController;
@@ -10,6 +11,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'index'])->name('login');
 Route::post('/', [AuthController::class, 'login_proses']);
+
+Route::middleware(['auth', 'cekLevel:superadmin,'])->group(function(){
+  /** 
+     * ini routing untuk pegawai controller!
+     */
+    Route::controller(pegawaiController::class)->group(function(){
+
+      Route::get('/pegawai', 'index');
+
+      Route::post('/pegawai/add', 'store')->name('savePegawai');
+
+      Route::get('/pegawai/edit/{id}', 'edit'); 
+      Route::post('/pegawai/edit/{id}', 'update'); 
+
+      Route::get('/pegawai/{id}', 'destroy');
+      
+  });
+
+
+});
 
 Route::middleware(['auth', 'cekLevel:superadmin,admin'])->group(function(){
     /**
@@ -23,21 +44,7 @@ Route::middleware(['auth', 'cekLevel:superadmin,admin'])->group(function(){
     Route::get('/dashboard', [dashboardController::class, 'index']);
 
      
-    /** 
-     * ini routing untuk pegawai controller!
-     */
-    Route::controller(pegawaiController::class)->group(function(){
-
-        Route::get('/pegawai', 'index');
-
-        Route::post('/pegawai/add', 'store')->name('savePegawai');
-
-        Route::get('/pegawai/edit/{id}', 'edit'); 
-        Route::post('/pegawai/edit/{id}', 'update'); 
-
-        Route::get('/pegawai/{id}', 'destroy');
-        
-    });
+   
 
      /**
       * Ini route stok
@@ -56,6 +63,10 @@ Route::middleware(['auth', 'cekLevel:superadmin,admin'])->group(function(){
        /**
       * Ini route barang masuk
       */
+      Route::controller(barangMasukController::class)->group(function(){
+        Route::get('/barang-masuk', 'index');
+        Route::get('/barang-masuk/add', 'create');
+      });
 
        /**
       * Ini route barang keluar
